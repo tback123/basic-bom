d57293624af88a1e581a5d68ef57b60e6a563b51
Tom Killingback <tomkillingback@gmail.com>
2021-08-25 19:18:13 +1000

    update model to spell 'description' correctly

--- basic_bom_backend/db/schema.rb
+++ basic_bom_backend/db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_095756) do
+ActiveRecord::Schema.define(version: 2021_08_22_030716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "parts", force: :cascade do |t|
-    t.string "descrition"
+    t.string "description"
     t.boolean "drawing"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
