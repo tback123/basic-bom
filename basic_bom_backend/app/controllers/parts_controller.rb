b215adbb2f30fe5c1d1b8f27917e46db5329b1b8
Tom Killingback <tomkillingback@gmail.com>
2021-08-25 21:57:56 +1000

    basic imple destroy & add exception catchs

--- basic_bom_backend/app/controllers/parts_controller.rb
+++ basic_bom_backend/app/controllers/parts_controller.rb
@@ -1,5 +1,4 @@
 class PartsController < ApplicationController
-
     include Response
     
     # GET /parts
@@ -13,43 +12,51 @@ class PartsController < ApplicationController
         # Create and save a new part with the book paramaters
         @part = Part.create(part_params)
 
-        puts part_params
-
         if @part.valid?
-            # Send part back
             json_response(@part)
-            
         else
             json_response(@part.errors, 400)
         end
-
-        
     end
 
     # GET /parts/:id
     def show
-        @part = Part.find(params[:id])
-        json_response(@part)
+        begin
+            @part = Part.find(params[:id])
+            json_response(@part)
+        rescue => e
+            json_response(e, 400)
+        end
     end
 
     # PATCH /parts/:id
     # PUT   /parts/:id
     def update
-        puts part_params
-        @part = Part.find(params[:id])
-        @part.update(part_params)
-        @part.save()
-        json_response(@part)
+        begin
+            @part = Part.find(params[:id])
+            @part.update(part_params)
+            @part.save()
+            json_response(@part2)
+        rescue => e
+            json_response(e, 400)
+        end
     end
 
     # DELETE /parts/:id
     def destroy
-        not_implemented()
+        begin
+            @part = Part.find(params[:id])
+            @part.destroy
+            json_response("")
+        rescue => e
+            json_response(e, 400)
+        end 
+        
     end
 
     private
         def part_params
-            params.permit(:description, :drawing )
+            params.permit(:description, :drawing, :id, :created_at, :updated_at )
         end
 
 end
