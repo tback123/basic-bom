344192de154325344600653a3e9342343a3172e9
Tom Killingback <tomkillingback@gmail.com>
2021-09-01 13:57:49 +1000

    set cors to http not https

--- basic_bom_backend/config/initializers/cors.rb
+++ basic_bom_backend/config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://localhost:3001'
+    origins 'http://localhost:3000'
 
     resource '*',
       headers: :any,
