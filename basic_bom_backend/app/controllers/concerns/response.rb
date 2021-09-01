d50835d5c406b457a4d6e32103012565d6025849
Tom Killingback <tomkillingback@gmail.com>
2021-09-01 14:40:35 +1000

    fix indentation

--- basic_bom_backend/app/controllers/concerns/response.rb
+++ basic_bom_backend/app/controllers/concerns/response.rb
@@ -1,10 +1,10 @@
 # app/controllers/concerns/response.rb
 module Response
-    def json_response(object, status = :ok)
-      render json: object, status: status
-    end
-
-    def not_implemented()
-      json_response("Not implemented", :not_implemented)
-    end
+  def json_response(object, status = :ok)
+    render json: object, status: status
   end
+
+  def not_implemented()
+    json_response("Not implemented", :not_implemented)
+  end
+end
