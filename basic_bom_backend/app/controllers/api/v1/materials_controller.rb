29f7e3f663abb342ce116d7a70955396b5c5050f
Tom Killingback <tomkillingback@gmail.com>
2021-09-08 12:41:28 +1000

    use default crud functions

--- basic_bom_backend/app/controllers/api/v1/materials_controller.rb
+++ basic_bom_backend/app/controllers/api/v1/materials_controller.rb
@@ -1,75 +1,34 @@
 class Api::V1::MaterialsController < ApplicationController
     include Response
+    include CRUD
         
     # GET /materials
     # If no parameters corresponding to a material attribute, return list of all materials
     # If there is one or more possible_material_params, return list all materials matching search criteria
     def index
-        begin
-            if possible_material_params.empty?
-                @materials = Material.all
-                json_response(@materials)
-            else
-                @materials = Material.where(possible_material_params)
-                json_response(@materials)
-            end
-        rescue => e
-            json_response(e, :bad_request)
-        end
-        
+        default_index(possible_material_params, Material)
     end
 
     # POST /materials
     def create
-        begin
-            # Create and save a new part with the book paramaters
-            @material = Material.create(create_material_params)
-
-            if @material.valid?
-                json_response(@material)
-            else
-                json_response(@material.errors, :bad_request)
-            end
-        rescue => e
-            json_response(e, :bad_request)
-        end
-
+        default_create(create_material_params, Material)
     end
 
     # GET /materials/:id
     def show
-        begin
-            @material = Material.find(params[:id])
-            json_response(@material)
-        rescue => e
-            json_response(e, :bad_request)
-        end
+        default_show(params[:id], Material)
     end
 
     # PATCH /materials/:id
     # PUT   /materials/:id
     # TODO: Deal with case where search parameter matches with multiple materials
     def update
-        begin
-            @material = Material.first(possible_material_params)
-            @material.update(possible_material_params)
-            @material.save()
-            json_response(@material)
-        rescue => e
-            json_response(e, :bad_request)
-        end
+        default_update(params[:id], possible_material_params, Material)
     end
 
     # DELETE /materials/:id
     def destroy
-        begin
-            @material = Material.find(params[:id])
-            @material.destroy
-            json_response("status"=>"success")
-        rescue => e
-            json_response(e, :bad_request)
-        end 
-        
+        default_destroy(params[:id], Material)
     end
 
     # GET /materials/:material_id/parts
@@ -79,7 +38,7 @@ class Api::V1::MaterialsController < ApplicationController
             @parts = @material.parts
             json_response(@parts)
         rescue => e
-            json_response(e, :bad_request)
+            json_string_error_response(e)
         end
     end
 
