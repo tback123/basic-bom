b8be8a1fc54442d6b4d3fc44898494e30949c4a5
Tom Killingback <tomkillingback@gmail.com>
2021-09-10 19:48:56 +1000

    initial add part form

--- basic_bom_frontend/src/components/AddPart.js
+++ basic_bom_frontend/src/components/AddPart.js
@@ -1,26 +1,20 @@
-import { Button, Paper, Typography, useTheme } from "@material-ui/core";
-import { Dialog, DialogTitle } from "@material-ui/core";
-import { Input, InputLabel, FormHelperText } from "@material-ui/core"
-import { FormControl } from '@material-ui/core';
+import { Button, Paper, Typography, Box, useTheme } from "@material-ui/core";
+import { TextField, ButtonGroup, RadioGroup, Radio, FormControlLabel, FormLabel } from "@material-ui/core";
+import { Dialog, DialogTitle, FormControl, Checkbox } from "@material-ui/core";
 
 import FormInputDropdown from "./form/FormInputDropdown"
 import FormInputRadio from "./form/FormInputRadio"
 
 
 
-import { FormProvider, useForm } from "react-hook-form";
+import { Controller, FormProvider, useForm } from "react-hook-form";
+import { useState } from "react";
 
-
-const defaultValues = {
-    dropdownValue: "",
-};
 
 function AddPart(props) {
     const theme = useTheme();
 
-    const methods = useForm({ defaultValues: defaultValues });
-    const { handleSubmit, reset, control, setValue, watch } = methods;
-    const onSubmit = (data) => console.log(data);
+    const { description, setDescriptionState } = useState("");
 
     const { addPart, onClose, open } = props;
 
@@ -30,70 +24,58 @@ function AddPart(props) {
 
     return (
         <Dialog onClose={handleClose} open={open}>
-            <Paper>
+            <Box display="flex" justifyContent="center" flexDirection="column" style={{ margin: theme.spacing(1) }}>
+                <form>
+                    <div>
+                        <TextField variant="outlined" id="description" label="description" />
+                    </div>
+                    <div>
+                        <Typography>Has this part got a drawing?</Typography>
+                        <ButtonGroup  color="primary">
+                            <Button variant="contained">Yes</Button>
+                            <Button variant="outlined">No</Button>
+                        </ButtonGroup>
+                    </div>
+                    <div>
+                        <Typography>Type</Typography>
+                        <ButtonGroup  color="primary">
+                            <Button variant="contained">Component</Button>
+                            <Button variant="outlined">Assembly</Button>
+                            <Button variant="outlined">Installation</Button>
+                        </ButtonGroup>
+                    </div>
+                    <div>
+                        <Typography>Source</Typography>
+                        <ButtonGroup  color="primary">
+                            <Button variant="contained">Internal</Button>
+                            <Button variant="outlined">External</Button>
+                        </ButtonGroup>
+                    </div>
+                    <div>
+                        <TextField variant="outlined" id="part_num" label="part_num" />
+                    </div>
+                    <div>
+                        <TextField variant="outlined" id="revision" label="revision" />
+                    </div>
+                    <div>
+                        <TextField variant="outlined" id="qty_per" label="qty_per" />
+                    </div>
+                    <div>
+                        <TextField variant="outlined" id="qty_to_order" label="qty_to_order" />
+                    </div>
+                    <div>
+                        <Typography>Supplier Placeholder</Typography>
+                    </div>
+                    <div>
+                        <TextField variant="outlined" id="comments" label="comments" />
+                    </div>
 
 
+                </form>
 
-                <FormInputDropdown
-                    name="dropdownValue"
-                    control={control}
-                    label="Dropdown Input"
-                    options={[
-                        {
-                            "label": "option 1",
-                            "value": 1,
-                        },
-                        {
-                            "label": "option 2",
-                            "value": 2,
-                        },
-                    ]}
-                />
+            </Box>
 
-                <FormInputRadio
-                    name={"radioValue"}
-                    control={control}
-                    label={"Radio Input"}
-                    options={[
-                        {
-                            "label": "option 1",
-                            "value": 1,
-                        },
-                        {
-                            "label": "option 2",
-                            "value": 2,
-                        },
-                    ]}
-                />
-
-
-
-                <FormControl>
-                    <InputLabel htmlFor="description">Description</InputLabel>
-                    <Input id="description-input" aria-describedby="Input a description of your part here" />
-
-                    <InputLabel htmlFor="drawing">Description</InputLabel>
-                    <Input id="description" aria-describedby="Input a description of your part here" />
-
-                    {/* <FormHelperText id="my-helper-text">HELLO</FormHelperText> */}
-                </FormControl>
-
-
-
-                <DialogTitle id="simple-dialog-title">Add part</DialogTitle>
-                <br />
-                <Typography varient="h2" component="h2" gutterBottom>
-                    Select what you would like to do, add up to 100 parts and then close the box
-                </Typography>
-                <br />
-                <Button style={{ margin: theme.spacing(1) }} variant="contained" size="small" onClick={addPart} color="primary"> Add Part </Button>
-                <Button style={{ margin: theme.spacing(1) }} variant="contained" size="small" onClick={handleClose} color="secondary"> Close </Button>
-                <br />
-
-            </Paper>
-
-
-        </Dialog>
+        </Dialog >
     );
 }
 
