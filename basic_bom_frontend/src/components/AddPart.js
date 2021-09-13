6762a3909398ad85c7c72b9737cb65323cb09f1e
Tom Killingback <tomkillingback@gmail.com>
2021-09-13 17:20:57 +1000

    implement basic formik form

--- basic_bom_frontend/src/components/AddPart.js
+++ basic_bom_frontend/src/components/AddPart.js
@@ -1,20 +1,26 @@
 import { Button, Paper, Typography, Box, useTheme } from "@material-ui/core";
-import { TextField, ButtonGroup, RadioGroup, Radio, FormControlLabel, FormLabel } from "@material-ui/core";
+import { TextField, ButtonGroup } from "@material-ui/core";
 import { Dialog, DialogTitle, FormControl, Checkbox } from "@material-ui/core";
+
+import { ToggleButtonGroup, ToggleButton } from '@material-ui/lab'
 
 import FormInputDropdown from "./form/FormInputDropdown"
 import FormInputRadio from "./form/FormInputRadio"
 
-
-
-import { Controller, FormProvider, useForm } from "react-hook-form";
 import { useState } from "react";
 
+import { useFormik, Form } from 'formik';
+import * as yup from 'yup'
+
+const validationSchema = yup.object({
+    description: yup
+        .string('Enter a description of the part')
+        .required('A description is required'),
+    has_drawing: yup.boolean()
+});
 
 function AddPart(props) {
     const theme = useTheme();
-
-    const { description, setDescriptionState } = useState("");
 
     const { addPart, onClose, open } = props;
 
@@ -22,21 +28,57 @@ function AddPart(props) {
         onClose();
     };
 
+    const formik = useFormik({
+        initialValues: {
+            description: '',
+            has_drawing: 'true'
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+            console.log(values)
+        },
+        onReset: (values) => {
+            alert(JSON.stringify(values, null, 2));
+        }
+
+    });
+
     return (
         <Dialog onClose={handleClose} open={open}>
             <Box display="flex" justifyContent="center" flexDirection="column" style={{ margin: theme.spacing(1) }}>
-                <form>
+
+                <form onSubmit={formik.handleSubmit}>
                     <div>
-                        <TextField variant="outlined" id="description" label="description" />
+                        <TextField
+                            variant="outlined"
+                            id="description"
+                            name="description"
+                            label="Description"
+                            value={formik.values.description}
+                            onChange={formik.handleChange}
+                            error={formik.touched.email && Boolean(formik.errors.email)}
+                            helperText={formik.touched.email && formik.errors.email} />
                     </div>
                     <div>
                         <Typography>Has this part got a drawing?</Typography>
-                        <ButtonGroup  color="primary">
-                            <Button variant="contained">Yes</Button>
-                            <Button variant="outlined">No</Button>
-                        </ButtonGroup>
+                        <ToggleButtonGroup 
+                            id="has_drawing"
+                            value={formik.values.has_drawing}
+                            onChange={formik.handleChange}
+                            aria-label="text formatting">
+                            <ToggleButton value="true" aria-label="yes">
+                                Yes
+                            </ToggleButton>
+                            <ToggleButton value="false" aria-label="no">
+                                No
+                            </ToggleButton>
+
+                        </ToggleButtonGroup>
+
+
                     </div>
-                    <div>
+                    {/* <div>
                         <Typography>Type</Typography>
                         <ButtonGroup  color="primary">
                             <Button variant="contained">Component</Button>
@@ -68,6 +110,9 @@ function AddPart(props) {
                     </div>
                     <div>
                         <TextField variant="outlined" id="comments" label="comments" />
+                    </div> */}
+                    <div>
+                        <Button variant="contained" type="submit">Submit</Button>
                     </div>
 
 
