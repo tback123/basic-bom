b882f56db823a9c5695260ab8933f4c38efb2420
Tom Killingback <tomkillingback@gmail.com>
2022-01-13 12:34:19 +1100

    update swagger docs

--- basic_bom_backend/spec/requests/api/v1/parts_spec.rb
+++ basic_bom_backend/spec/requests/api/v1/parts_spec.rb
@@ -1,136 +1,28 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/parts', type: :request do
-
-  path '/api/v1/parts/{part_id}/children' do
-    # You'll want to customize the parameter types...
-    parameter name: 'part_id', in: :path, type: :integer, description: 'part_id'
-
-    get('children part') do
-      response(200, 'successful') do
-        let(:part_id) { '1' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
-    put('children_add part') do
-      response(200, 'successful') do
-        let(:part_id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
-    delete('children_delete part') do
-      response(200, 'successful') do
-        let(:part_id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-  end
-
-  path '/api/v1/parts/{part_id}/parents' do
-    # You'll want to customize the parameter types...
-    parameter name: 'part_id', in: :path, type: :string, description: 'part_id'
-
-    get('parents part') do
-      response(200, 'successful') do
-        let(:part_id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
-    put('parents_add part') do
-      response(200, 'successful') do
-        let(:part_id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
-    delete('parents_delete part') do
-      response(200, 'successful') do
-        let(:part_id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-  end
-
+  
   path '/api/v1/parts' do
-
+  
     get('list parts') do
-      response(200, 'successful') do
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+      response '200', 'Parts listed' do
+        let(:parts) { { title: 'foo', content: 'bar' } }
+      
+        before do |example|
+          submit_request(example.metadata)
         end
-        run_test!
+      
+        it 'returns a valid 201 response' do |example|
+          assert_response_matches_metadata(example.metadata)
+        end
       end
-    end
 
+    end
+  
     post('create part') do
-      response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
     end
+     
   end
 
   path '/api/v1/parts/{id}' do
@@ -138,63 +30,47 @@ RSpec.describe 'api/v1/parts', type: :request do
     parameter name: 'id', in: :path, type: :string, description: 'id'
 
     get('show part') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
     end
 
     patch('update part') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
     end
 
     put('update part') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
     end
 
     delete('delete part') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
     end
+      
   end
+
+  path '/api/v1/parts/{part_id}/children' do
+    # You'll want to customize the parameter types...
+    parameter name: 'part_id', in: :path, type: :integer, description: 'part_id'
+    
+    get('children part') do
+    end
+
+    put('children_add part') do
+    end
+
+    delete('children_delete part') do
+    end
+    
+  end
+
+  path '/api/v1/parts/{part_id}/parents' do
+    # You'll want to customize the parameter types...
+    parameter name: 'part_id', in: :path, type: :string, description: 'part_id'
+
+    get('parents part') do
+    end
+
+    put('parents_add part') do
+    end
+
+    delete('parents_delete part') do
+    end
+     
+  end
+
 end
