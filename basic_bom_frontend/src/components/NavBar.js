785a7a8f4ba0ebcc35d197e86d27d0dd0640a5c4
Tom Killingback <tomkillingback@gmail.com>
2022-01-14 17:08:28 +1100

    added Navbar

--- basic_bom_frontend/src/components/NavBar.js
+++ basic_bom_frontend/src/components/NavBar.js
@@ -9,6 +9,21 @@ import {
 import Menu from "@material-ui/icons/Menu";
 import { useState } from "react";
 
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import ExtensionIcon from '@mui/icons-material/Extension';
+import PlaceIcon from '@mui/icons-material/Place';
+import StoreIcon from '@mui/icons-material/Store';
+import BlurOnIcon from '@mui/icons-material/BlurOn';
+
+import { spacing } from '@mui/system';
+
 
 function Navbar(props) {
   const theme = useTheme();
@@ -18,6 +33,65 @@ function Navbar(props) {
   const toggleNavBar = () => {
     setIsNavOpen(!isNavOpen);
   }
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+    >
+      <List>
+        <ListItem key="test">
+          <ListItemText primary="Navigation" />
+        </ListItem>
+
+      </List>
+
+      <Divider />
+
+      <List>
+        <ListItem button key="Parts List">
+          <ListItemIcon>
+            <ExtensionIcon />
+          </ListItemIcon>
+          <ListItemText primary="Parts List" />
+        </ListItem>
+
+
+        <ListItem button key="Suppliers">
+          <ListItemIcon>
+            <StoreIcon />
+          </ListItemIcon>
+          <ListItemText primary="Suppliers" />
+        </ListItem>
+
+        <ListItem button key="Locations">
+          <ListItemIcon>
+            <PlaceIcon />
+          </ListItemIcon>
+          <ListItemText primary="Locations" />
+        </ListItem>
+
+        <ListItem button key="Materials">
+          <ListItemIcon>
+            <BlurOnIcon />
+          </ListItemIcon>
+          <ListItemText primary="Materials" />
+        </ListItem>
+
+      </List>
+
+      <Box mt={3} position="absolute" bottom="0px">
+        <List mt={5} >
+          <ListItem>
+            <ListItemText>Software written by Tom Killingback. github.com/tback123
+            </ListItemText>
+          </ListItem>
+
+        </List>
+      </Box>
+
+    </Box>
+  );
 
   return (
     <>
@@ -38,13 +112,13 @@ function Navbar(props) {
 
 
       </AppBar>
-      
+
       <Drawer
         anchor={'left'}
         open={isNavOpen}
         onClose={toggleNavBar}
       >
-        hi
+        {list("left")}
       </Drawer>
     </>
   );
