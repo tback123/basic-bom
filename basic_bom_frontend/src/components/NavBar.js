3e61d48fa68b3d9c67e760891d87467b71687feb
Tom Killingback <tomkillingback@gmail.com>
2022-01-14 13:59:18 +1100

    basic sivbar

--- basic_bom_frontend/src/components/NavBar.js
+++ basic_bom_frontend/src/components/NavBar.js
@@ -2,23 +2,31 @@ import {
   AppBar,
   Toolbar,
   Typography,
-  Button,
   IconButton,
-  Grid,
-  useTheme
+  useTheme,
+  Drawer
 } from "@material-ui/core";
 import Menu from "@material-ui/icons/Menu";
+import { useState } from "react";
 
 
 function Navbar(props) {
   const theme = useTheme();
 
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNavBar = () => {
+    setIsNavOpen(!isNavOpen);
+  }
+
   return (
+    <>
       <AppBar position="static" color="primary">
         <Toolbar >
           <IconButton
             edge="start"
             aria-label="menu"
+            onClick={toggleNavBar}
           >
             <Menu />
           </IconButton>
@@ -26,7 +34,19 @@ function Navbar(props) {
             A Basic Bill of Materials
           </Typography>
         </Toolbar>
+
+
+
       </AppBar>
+      
+      <Drawer
+        anchor={'left'}
+        open={isNavOpen}
+        onClose={toggleNavBar}
+      >
+        hi
+      </Drawer>
+    </>
   );
 };
 
